b2d3ce0fdcd84734d27bd348e31517f471b8a7db
dhruvdev01 <72223087+dhruvdev01@users.noreply.github.com>
2022-10-10 22:06:38 +0530

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Tutorial />
+    <div class="wrapper">
+      <h2 class="head">Hello <span class="head__inner-el"> World</span></h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100vh;
+  .head {
+    color: red;
+    &__inner-el {
+      color: skyblue;
+    }
+  }
+}
+</style>
